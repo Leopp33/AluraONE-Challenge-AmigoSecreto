5292f701ec5a2405ecfdece163fc7c960faf82e7
Leopoldo Jesús Pérez Paxtián <lperezp@clb.santillana.com>
2025-08-08 22:51:31 -0600

    feature: se crearon y ejecutaron todos los cambios requeridos

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # AluraONE-Challenge-AmigoSecreto
 Challenge: Amigo Secreto; Práctica para habilidades de lógica y programación con Javascript.
+
+# Desafíos Enfrentados
+En éste sencillo (aparentemente) desafío, fue realmente interesante que al avanzar en una funcionalidad otra no estuviera cubierta y que al resolver lo requerido aparecieron funcionalidades no contempladas pero que era necesario cubrir por experiencia como usuario.
+
+Y éstos fueron:
++ Que no se añadieran elementos Li (extras).
++ Que no permitiera jugar sin ingresar al menos 2 jugadores.
++ Que al volver a jugar, reiniciara la lista acumuladora de participantes.
+
+Luego por querer lucirme con el confetti:
++ No crear, crear y crear etiquetas "img".
++ Ajustar apariencia.
++ Retirar al momento de jugar de nuevo.
+
+# Agradecimiento
+Realmente me siento muy agradecido por la oportunidad. Gracias Alura. Gracias Oracle por el programa ONE. Ya estoy ansioso por las siguientes etapas.
+
+# Derechos de Autor
+Respecto a los Derechos de Autor de la imágen utilizada alusiva a celebración: https://pixabay.com/gifs/congratulations-party-confetti-7600/ está bajo licenciamiento de uso libre, así que, siéntete libre de utilizarla ;)
--- app.js
+++ app.js
@@ -1,15 +1,58 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigo = document.getElementById("amigo");
+const img = document.createElement('img');
+const lista_amigos = [];
+const seccion_lista_amigos = document.getElementById("listaAmigos");
+const seccion_amigos_sorteo = document.getElementById("resultado");
+const seccion_input = document.querySelector('.input-section');
 
-// Agregar nombre a una lista.
-// 1. Verificar si contiene texto el campo.
-// 2. Agregar nombre a la lista.
-// 3. Imprimir en pantalla (añadir nodo al DOM que contenga la lista a visualizar).
 function agregarAmigo(){
-    console.log("Agregar amigo, tarea corriendo...")
+ 
+    if (amigo.value){
+        lista_amigos.push(amigo.value);
+        
+        seccion_lista_amigos.innerHTML = "";
+
+        lista_amigos.forEach(crear_elementos_li);
+    }
+    else{
+        alert("Para continuar ingresa un nombre en el campo designado.");
+    }
+    
+    function crear_elementos_li(amigo_lista){
+      
+        const li = document.createElement("li");
+        li.textContent = amigo_lista
+        seccion_lista_amigos.appendChild(li);
+        amigo.value = "";
+    }
+
+    seccion_input.removeChild(img);
+    seccion_amigos_sorteo.innerHTML = null;
+    
 }
 
-// Recibir Lista, Medir longitud (para usar como límite), 
-// usar Math.random, retornar un nombre de la lista.
 function sortearAmigo(){
-    console.log("Sortear Amigo, tarea en reposo...")
-}
+
+    if(lista_amigos.length >= 2){
+
+        seleccion_random = Math.floor( Math.random() * lista_amigos.length);
+
+        seccion_amigos_sorteo.innerHTML = lista_amigos[seleccion_random];
+
+        seccion_lista_amigos.innerHTML = null;
+
+        lista_amigos.length = 0;
+
+        
+        img.src = 'assets/congratulations.gif';
+        img.alt = 'Imágen de celebración: Confetti Animado';
+        img.className = 'celebracion_confetti';
+        seccion_input.appendChild(img);
+        
+
+    }
+    else{
+        alert("Debes agregar por lo menos a dos participantes para jugar.")
+    }
+    
+} 
