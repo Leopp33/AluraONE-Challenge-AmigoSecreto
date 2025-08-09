8401eea6f332d3ac77d3d41c8ca3894300f612af
Leopoldo Jesús Pérez Paxtián <lperezp@clb.santillana.com>
2025-08-08 19:11:42 -0600

    init: comprueba funcionamiento de enlace a Javascript.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Agregar nombre a una lista.
+// 1. Verificar si contiene texto el campo.
+// 2. Agregar nombre a la lista.
+// 3. Imprimir en pantalla (añadir nodo al DOM que contenga la lista a visualizar).
+function agregarAmigo(){
+    console.log("Agregar amigo, tarea corriendo...")
+}
+
+// Recibir Lista, Medir longitud (para usar como límite), 
+// usar Math.random, retornar un nombre de la lista.
+function sortearAmigo(){
+    console.log("Sortear Amigo, tarea en reposo...")
+}
